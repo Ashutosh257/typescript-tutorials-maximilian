488414eb2525b0b9997f739a5317f8a5ecdbbbcc
Ashutosh <theflash257@gmail.com>
2020-09-09 07:20:43 +0530

    restructured

--- lec-9/app.js
+++
@@ -1,2 +0,0 @@
-console.log("Your code goes here!!");
-console.log("Your code for Lec-9 here!!");
--- lec-9/app.ts
+++
@@ -1,3 +0,0 @@
-console.log("Your code goes here!!");
-
-console.log("Your code for Lec-9 here!!");
